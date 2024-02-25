a27910bedf9854d1781a20f811a211ad018496e6
AliEm <ali.emamzadeh12@gmail.com>
2024-02-25 14:33:04 +0330

    feat: Add Exceptions

---
+++ CA1/src/main/java/defines/Errors.java
@@ -0,0 +1,18 @@
+package defines;
+
+public class Errors {
+    public static final String USERNAME_ALREADY_TAKEN = "The username is already taken.";
+    public static final String EMAIL_ALREADY_TAKEN = "The email is already taken.";
+    public static final String INVALID_USER_TYPE = "User Type should be manager or client.";
+    public static final String INVALID_EMAIL_FORMAT = "The email is in invalid format.";
+    public static final String INVALID_USERNAME_FORMAT = "The username is in invalid format";
+    public static final String ADDRESS_SHOULD_CONTAINS_CITY_AND_COUNTRY = "The address should contains city and country";
+    public static final String RESTAURANT_NAME_ALREADY_TAKEN = "The restaurant name is already taken.";
+    public static final String INVALID_MANAGER_USERNAME = "The manager username is invalid";
+    public static final String TIME_OF_RESTAURANT_SHOULD_BE_ROUND = "The start time and end time of restaurant should be round hours";
+    public static final String ADDRESS_SHOULD_CONTAINS_CITY_AND_COUNTRY_AND_STREET = "The address should contains city and country and street";
+    public static final String TABLE_NUMBER_ALREADY_TAKEN = "The table number is already taken in this restaurant";
+    public static final String INVALID_RESTAURANT_NAME = "Invalid restaurant name";
+    public static final String INVALID_RESERVATION_NUMBER = "Invalid Reservation number";
+    public static final String CANNOT_CANCEL_RESERVATION_BECAUSE_OF_DATE = "The reservation cannot be cancelled because of date";
+}
