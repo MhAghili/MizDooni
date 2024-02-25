5b8f6cb7338461a849ed2d550a7e78d8d5549c9d
AliEm <ali.emamzadeh12@gmail.com>
2024-02-25 14:35:16 +0330

    feat: Add Some Utility class

---
+++ CA1/src/main/java/utils/ConsoleIOHandler.java
@@ -0,0 +1,18 @@
+package utils;
+
+import models.Request;
+
+import java.io.Console;
+
+public class ConsoleIOHandler {
+    public static Request getInput() {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] parts = line.split(" ", 2); // Split into two parts at the first occurrence of space
+        return new Request(parts[0], parts[1]);
+    }
+
+    public static void writeOutput(String response) {
+        System.out.println(response);
+    }
+}
